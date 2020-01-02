e9854201c2a2b3fd6efe969d8784cbd6a9f24055
MURATA Mitsuharu <hikari.photon+mygit@gmail.com>
2020-01-02 10:00:55 +0900

    new

--- matrix.js
+++ matrix.js
@@ -124,40 +124,112 @@ class Matrix extends Array{
         return tex
     }
 
+    get inv(){
+        return this.inverse
+    }
+
+
     get inverse(){
         let a = Matrix.rows(this)
         let e = Matrix.identity(a.row_size)
         let k, l
 
+        for(let i = 0; i < a.row_size; i++){
+            for(let j = 0; j < a.column_size; j++){
+                a[i][j] = new Rational(a[i][j], 1)
+                e[i][j] = new Rational(e[i][j], 1)
+            }
+        }
+
         for(let m = 0; m < a.row_size; m++){
-            k = a[m][m]
+            k = a[m][m].reciprocal
             for(let i = 0; i < a.column_size; i++){
-                a[m][i] /= k
-                e[m][i] /= k
+                a[m][i] = k.times(a[m][i])
+                e[m][i] = k.times(e[m][i])
             }
             for(let i = m + 1; i < a.row_size; i++){
-                l = a[i][m]
+                l = (new Rational).minus(a[i][m])
                 for(let j = 0; j < a.column_size; j++){
-                    a[i][j] = -l * a[m][j] + a[i][j] 
-                    e[i][j] = -l * e[m][j] + e[i][j]
+                    a[i][j] = (l.times(a[m][j])).plus(a[i][j]) 
+                    e[i][j] = (l.times(e[m][j])).plus(e[i][j]) 
                 }
             }
         }
 
         for(let m = a.row_size; m > 0; m--){
             for(let n = a.row_size - 1; n >= m; n--){
-                k = a[m - 1][n]
+                k = (new Rational).minus(a[m - 1][n])
                 for(let j = 0; j < a.column_size; j++){
-                    a[m - 1][j] = -k * a[n][j] + a[m - 1][j]
-                    e[m - 1][j] = -k * e[n][j] + e[m - 1][j]
+                    a[m - 1][j] = (k.times(a[n][j])).plus(a[m - 1][j])
+                    e[m - 1][j] = (k.times(e[n][j])).plus(e[m - 1][j])
                 }
             }
         }
+
+        for(let i = 0; i < a.row_size; i++){
+            for(let j = 0; j < a.column_size; j++){
+                e[i][j] = e[i][j].numerator / e[i][j].denominator
+            }
+        }
+
         return e
     }
+}
 
-    get inv(){
-        return this.inverse
+class Rational{
+    constructor(numerator = 0, denominator = 1){
+        this.numerator = numerator
+        this.denominator = denominator
     }
 
+    plus(n){
+        let r = new Rational
+        r.numerator = this.numerator * n.denominator + this.denominator * n.numerator
+        r.denominator = this.denominator * n.denominator
+        r.reduction()
+        return r
+    }
+
+    times(n){
+        let r = new Rational
+        r.numerator = this.numerator * n.numerator
+        r.denominator = this.denominator * n.denominator
+        r.reduction()
+        return r
+    }
+
+    get reciprocal(){
+        let r = new Rational
+        if(this.numerator > 0){
+            r.numerator = this.denominator
+            r.denominator = this.numerator
+        }else{
+            r.numerator = -this.denominator
+            r.denominator = -this.numerator
+        }
+        return r
+    }
+
+    minus(n){
+        let r = new Rational(-1, 1)
+        return r.times(n).plus(this)
+    }
+
+    reduction(){
+        let gcd = Rational.gcd(this.numerator, this.denominator)
+        this.numerator /= gcd
+        this.denominator /= gcd
+    }
+
+    static gcd(a, b){
+        let m, d
+        while(true){
+            if(b == 0) break
+            m = a % b
+            a = b
+            b = m
+        }
+        return a
+    }
 }
+
